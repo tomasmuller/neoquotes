6d6cfbe9d3c2caf9db4d0f4761b490e1fbe15ef1
Tomás Augusto Müller <tomas.am@gmail.com>
2012-02-17 11:33:44 -0200

    Stock repository tests.

--- src/test/java/com/herokuapp/neoquotes/AbstractUnitTest.java
+++ src/test/java/com/herokuapp/neoquotes/AbstractUnitTest.java
@@ -2,11 +2,15 @@ package com.herokuapp.neoquotes;
 
 import org.apache.log4j.Logger;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-@ContextConfiguration({"/neoquotes-test-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/neoquotes-test-context.xml"})
+@Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class AbstractUnitTest {
 
 	protected Logger logger = Logger.getLogger("neoquotes-tests");
--- src/test/java/com/herokuapp/neoquotes/repositories/StockRepositoryTest.java
+++ src/test/java/com/herokuapp/neoquotes/repositories/StockRepositoryTest.java
@@ -4,19 +4,46 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.herokuapp.neoquotes.AbstractUnitTest;
+import com.herokuapp.neoquotes.models.Stock;
+
+import static org.junit.Assert.assertEquals;
 
 @Transactional
 public class StockRepositoryTest extends AbstractUnitTest {
 
-	@Autowired
-	private StockRepository stockRepository;
-	
-	@Test
-	public void testStockRepositoryNotNull() {
-		assertNotNull(stockRepository);
-	}
-	
+    @Autowired
+    private Neo4jTemplate template;
+      
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Test
+    public void testStockRepositoryNotNull() {
+        assertNotNull(stockRepository);
+    }
+
+    @Test
+    public void createAndReadStock() {
+        final Stock stock = new Stock("NASDAQ");
+        final Stock saved = template.save(stock);
+        assertEquals("must be same name after save", stock.getName(), saved.getName());
+        final Stock loaded = template.findOne(saved.getId(), Stock.class);
+        assertEquals("same id after load", saved.getId(), loaded.getId());
+        assertEquals("same name after load", stock.getName(), saved.getName());
+    }
+
+    @Test
+    public void createAndReadStockWithRepository() {
+        final Stock stock = new Stock("NYSE");
+        final Stock saved = stockRepository.save(stock);
+        assertEquals("same name after save", stock.getName(), saved.getName());
+        final Stock loaded = stockRepository.findOne(saved.getId());
+        assertEquals("same id after load", saved.getId(), loaded.getId());
+        assertEquals("same name after load", stock.getName(), saved.getName());
+    }
+
 }
